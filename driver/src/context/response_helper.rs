2d4cd2a496208efa6a15a35e613877073f10dac8
Alexander <qrer71@yandex.ru>
2024-06-26 16:46:09 +0300

    Create response helper

---
+++ driver/src/context/response_helper.rs
@@ -0,0 +1,86 @@
+use axum::http::StatusCode;
+use axum::Json;
+use axum::response::{IntoResponse, Response};
+use serde::Serialize;
+use tracing::log::error;
+
+use crate::context::errors::AppError;
+
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct JsonErrorResponse {
+    error_code: String,
+    errors: Vec<String>,
+}
+
+impl JsonErrorResponse {
+    pub(crate) fn new(error_code: String, errors: Vec<String>) -> Self {
+        Self { error_code, errors }
+    }
+}
+
+impl IntoResponse for AppError {
+    fn into_response(self) -> Response {
+        match self {
+            AppError::Validation(validation_errors) => {
+                error!("{:?}", validation_errors);
+
+                let mut messages: Vec<String> = Vec::new();
+                let errors = validation_errors.field_errors();
+                for (_, v) in errors.into_iter() {
+                    for validation_error in v {
+                        if let Some(msg) = validation_error.clone().message {
+                            messages.push(msg.to_string());
+                        }
+                    }
+                }
+
+                (
+                    StatusCode::BAD_REQUEST,
+                    Json(JsonErrorResponse::new(
+                        "invalid_request".to_string(),
+                        messages,
+                    )),
+                )
+            }
+            AppError::JsonRejection(rejection) => {
+                error!("{:?}", rejection);
+
+                let messages = vec![rejection.to_string()];
+                (
+                    StatusCode::BAD_REQUEST,
+                    Json(JsonErrorResponse::new(
+                        "invalid_request".to_string(),
+                        messages,
+                    )),
+                )
+            }
+            AppError::ApiPathRejection(rejection) => {
+                error!("{:?}", rejection);
+
+                let messages = vec![rejection.to_string()];
+                (
+                    StatusCode::BAD_REQUEST,
+                    Json(JsonErrorResponse::new(
+                        "missing_api_version".to_string(),
+                        messages,
+                    )),
+                )
+            }
+            AppError::UnknownApiVerRejection(version) => {
+                let err = format!("Unknown api version ({}).", version);
+                error!("{}", err);
+
+                let messages = vec![err];
+                (
+                    StatusCode::NOT_FOUND,
+                    Json(JsonErrorResponse::new(
+                        "unknown_api_version".to_string(),
+                        messages,
+                    )),
+                )
+            }
+        }
+            .into_response()
+    }
+}
