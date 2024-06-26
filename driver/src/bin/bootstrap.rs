72ec8add402fdb3cc93439fffbb3e2c20d41f621
Alexander <qrer71@yandex.ru>
2024-06-26 16:48:18 +0300

    Create an entry point

---
+++ driver/src/bin/bootstrap.rs
@@ -0,0 +1,12 @@
+use std::sync::Arc;
+use driver::module::Modules;
+use driver::startup::startup;
+
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let modules = Modules::new().await;
+    let _ = startup(Arc::new(modules)).await;
+
+    Ok(())
+}
