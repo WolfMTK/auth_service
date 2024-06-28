199db163df64a63f29845a3a85177eadc62319d9
Alexander <qrer71@yandex.ru>
2024-06-28 21:29:46 +0300

    Add a method to update the user

--- adapter/src/repository/user.rs
+++ adapter/src/repository/user.rs
@@ -1,10 +1,10 @@
 use async_trait::async_trait;
 use sqlx::{query, query_as};
 
-use kernel::model::user::{NewUser, User};
+use kernel::model::user::{NewUser, UpdateUser, User};
 use kernel::repository::user::UserRepository;
 
-use crate::model::user::{InsertUser, StoredUser};
+use crate::model::user::{InsertUser, StoredUser, UpdateStoredUser};
 use crate::repository::DatabaseRepositoryImpl;
 
 #[async_trait]
@@ -20,7 +20,6 @@ impl UserRepository for DatabaseRepositoryImpl<User> {
             values
                 ($1, $2, $3, $4)
         "#;
-
         let _ = query(insert_sql)
             .bind(user.id)
             .bind(user.username)
@@ -45,6 +44,47 @@ impl UserRepository for DatabaseRepositoryImpl<User> {
             .bind(id)
             .fetch_one(&*pool)
             .await?;
+        Ok(stored_user.try_into()?)
+    }
+
+    async fn update(&self, source: UpdateUser) -> anyhow::Result<User> {
+        let pool = self.db.0.clone();
+        let user: UpdateStoredUser = source.into();
+        let id = user.id.clone();
+        let update_sql = r#"
+            update
+                users as target
+            set
+                username = case when $2 is not null then $2 else target.username end,
+                email = case when $3 is not null then $3 else target.email end,
+                password = case when $4 is not null then $4 else target.password end
+            where
+                target.id = $1
+        "#;
+        let _ = query(update_sql)
+            .bind(user.id)
+            .bind(user.username)
+            .bind(user.email)
+            .bind(user.password)
+            .execute(&*pool)
+            .await?;
+
+        let get_sql = r#"
+            select
+                u.id as id,
+                u.username as username,
+                u.email as email,
+                u.password as password
+            from
+                users as u
+            where
+                u.id = $1
+        "#;
+        let stored_user = query_as::<_, StoredUser>(get_sql)
+            .bind(id)
+            .fetch_one(&*pool)
+            .await?;
+
         Ok(stored_user.try_into()?)
     }
 }
