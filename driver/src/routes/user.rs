cb20577beea9eb64a937dd73276ec8a2e223b6eb
Alexander <qrer71@yandex.ru>
2024-06-28 21:31:01 +0300

    Add endpoints

--- driver/src/routes/user.rs
+++ driver/src/routes/user.rs
@@ -1,20 +1,21 @@
 use std::sync::Arc;
-use tracing::log::{error, info};
-use axum::extract::State;
+
+use axum::extract::{Path, State};
 use axum::http::StatusCode;
 use axum::Json;
 use axum::response::IntoResponse;
+use tracing::log::{error, info};
+
 use crate::context::api_version::ApiVersion;
 use crate::context::validate::ValidatedRequest;
-use crate::model::user::{JsonCreateUser, JsonUser};
-
+use crate::model::user::{JsonCreateUser, JsonUpdateUser, JsonUser};
 use crate::module::{Modules, ModulesExt};
 
 pub async fn create_user(
     _: ApiVersion,
     modules: State<Arc<Modules>>,
     ValidatedRequest(source): ValidatedRequest<JsonCreateUser>,
-) -> Result<impl IntoResponse, StatusCode>{
+) -> Result<impl IntoResponse, StatusCode> {
     let resp = modules.user_use_case().create_user(source.into()).await;
 
     resp.map(|view| {
@@ -26,4 +27,50 @@ pub async fn create_user(
             error!("{:?}", err);
             StatusCode::INTERNAL_SERVER_ERROR
         })
+}
+
+pub async fn update_user(
+    _: ApiVersion,
+    Path((_v, id)): Path<(ApiVersion, String)>,
+    modules: State<Arc<Modules>>,
+    ValidatedRequest(source): ValidatedRequest<JsonUpdateUser>,
+) -> Result<impl IntoResponse, impl IntoResponse> {
+    let resp = modules
+        .user_use_case()
+        .update_user(source.to_view(id))
+        .await;
+    resp.map(|view| {
+        info!("Update user: {}", view.id);
+        let json: JsonUser = view.into();
+        (StatusCode::OK, Json(json))
+    })
+        .map_err(|err| {
+            println!("{}", err);
+            error!("{:?}", err);
+            StatusCode::INTERNAL_SERVER_ERROR
+        })
+}
+
+pub async fn get_user() {
+    todo!("")
+}
+
+pub async fn get_users() {
+    todo!()
+}
+
+pub async fn delete_user() {
+    todo!()
+}
+
+pub async fn get_me() {
+    todo!()
+}
+
+pub async fn update_me() {
+    todo!()
+}
+
+pub async fn delete_me() {
+    todo!()
 }
