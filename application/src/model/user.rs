69e1ff4c6181b1513d52c9175216154fb52d811a
Alexander <qrer71@yandex.ru>
2024-06-28 21:30:03 +0300

    Add model UpdateUserView

--- application/src/model/user.rs
+++ application/src/model/user.rs
@@ -1,5 +1,5 @@
-use kernel::model::Id;
 use kernel::model::user::{NewUser, User};
+use kernel::model::Id;
 
 pub struct UserView {
     pub id: String,
@@ -37,6 +37,34 @@ impl TryFrom<CreateUser> for NewUser {
     type Error = anyhow::Error;
 
     fn try_from(value: CreateUser) -> Result<Self, Self::Error> {
-        Ok(NewUser::new(Id::gen(), value.username, value.email, value.password))
+        Ok(NewUser::new(
+            Id::gen(),
+            value.username,
+            value.email,
+            value.password,
+        ))
+    }
+}
+
+pub struct UpdateUserView {
+    pub id: String,
+    pub username: Option<String>,
+    pub email: Option<String>,
+    pub password: Option<String>,
+}
+
+impl UpdateUserView {
+    pub fn new(
+        id: String,
+        username: Option<String>,
+        email: Option<String>,
+        password: Option<String>,
+    ) -> Self {
+        Self {
+            id,
+            username,
+            email,
+            password,
+        }
     }
 }
