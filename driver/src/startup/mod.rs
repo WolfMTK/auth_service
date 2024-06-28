84cabf57db169b84583391af286f122ff2589584
Alexander <qrer71@yandex.ru>
2024-06-28 22:25:07 +0300

    Add getting host and port from environment variables

--- driver/src/startup/mod.rs
+++ driver/src/startup/mod.rs
@@ -1,7 +1,9 @@
+use std::env;
+use std::net::{IpAddr, SocketAddr};
 use std::sync::Arc;
 
-use axum::Router;
 use axum::routing::{get, post};
+use axum::Router;
 use tokio::net::TcpListener;
 
 use crate::module::Modules;
@@ -16,11 +18,27 @@ pub async fn startup(modules: Arc<Modules>) {
         .nest("/:v/users", user_router)
         .with_state(modules);
 
-    let listener = TcpListener::bind("0.0.0.0:8000")
+    let addr = SocketAddr::from(init_addr());
+    let listener = TcpListener::bind(addr)
         .await
         .unwrap_or_else(|_| panic!("TcpListener cannot bind"));
+    tracing::info!("Server listening on {}", addr);
 
     axum::serve(listener, app)
         .await
         .unwrap_or_else(|_| panic!("Server cannot launch"));
+}
+
+fn init_addr() -> (IpAddr, u16) {
+    let ip_addr = match env::var("HOST") {
+        Ok(val) => val.parse::<IpAddr>(),
+        Err(_) => "127.0.0.1".parse::<IpAddr>(),
+    }.expect("HOST is invalid.");
+
+    let port = match env::var("PORT") {
+        Ok(val) => val.parse::<u16>(),
+        Err(_) => Ok(8000)
+    }.expect("PORT is invalid.");
+
+    (ip_addr, port)
 }
