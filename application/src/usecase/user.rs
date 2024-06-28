6e04c3e85233fc010ee16e416b7e4683d1ac48a4
Alexander <qrer71@yandex.ru>
2024-06-28 21:30:45 +0300

    Add a method to update the user

--- application/src/usecase/user.rs
+++ application/src/usecase/user.rs
@@ -1,22 +1,41 @@
 use std::sync::Arc;
 
 use adapter::modules::RepositoriesModuleExt;
-use kernel::repository::user::UserRepository;
+use kernel::{model::user::UpdateUser, repository::user::UserRepository};
 
-use crate::model::user::{CreateUser, UserView};
+use crate::model::user::{CreateUser, UpdateUserView, UserView};
 
 pub struct UserUseCase<R: RepositoriesModuleExt> {
     repositories: Arc<R>,
 }
 
 impl<R: RepositoriesModuleExt> UserUseCase<R> {
-    pub fn new(repositories: Arc<R>) -> Self { Self { repositories } }
+    pub fn new(repositories: Arc<R>) -> Self {
+        Self { repositories }
+    }
 
     pub async fn create_user(&self, source: CreateUser) -> anyhow::Result<UserView> {
         let user_view = self
             .repositories
             .user_repository()
             .insert(source.try_into()?)
+            .await?;
+
+        Ok(user_view.into())
+    }
+
+    pub async fn update_user(&self, source: UpdateUserView) -> anyhow::Result<UserView> {
+        let update_user = UpdateUser::new(
+            source.id.try_into()?,
+            source.username,
+            source.email,
+            source.password,
+        );
+
+        let user_view = self
+            .repositories
+            .user_repository()
+            .update(update_user)
             .await?;
 
         Ok(user_view.into())
--- kernel/src/repository/user.rs
+++ kernel/src/repository/user.rs
@@ -1,8 +1,9 @@
 use async_trait::async_trait;
 
-use crate::model::user::{NewUser, User};
+use crate::model::user::{NewUser, UpdateUser, User};
 
 #[async_trait]
 pub trait UserRepository {
     async fn insert(&self, source: NewUser) -> anyhow::Result<User>;
+    async fn update(&self, source: UpdateUser) -> anyhow::Result<User>;
 }
