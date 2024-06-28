b989341f9cde4353c9a54937d8c4a97d1f15e1cc
Alexander <qrer71@yandex.ru>
2024-06-28 16:14:17 +0300

    Add function to validate the email

--- driver/src/model/mod.rs
+++ driver/src/model/mod.rs
@@ -1,1 +1,12 @@
+use regex::Regex;
+
 pub mod user;
+pub mod email;
+
+const EMAIL_PATTERN: &str = r"^([a-z0-9_+]([a-z0-9_+.]*[a-z0-9_+])?)@([a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,6})";
+
+pub fn validate_email(email: &str) -> bool {
+    let pattern = Regex::new(EMAIL_PATTERN).unwrap();
+
+    pattern.is_match(email)
+}
