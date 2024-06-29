0df26f4b4d162824d2bffcd61eb8af8237f49568
Alexander <qrer71@yandex.ru>
2024-06-29 13:13:45 +0300

    Add a method for deleting user data

--- kernel/src/repository/user.rs
+++ kernel/src/repository/user.rs
@@ -1,8 +1,8 @@
 use async_trait::async_trait;
 
 use crate::model::{
-    Id,
     user::{NewUser, UpdateUser, User},
+    Id,
 };
 
 #[async_trait]
@@ -10,4 +10,5 @@ pub trait UserRepository {
     async fn get(&self, id: &Id<User>) -> anyhow::Result<Option<User>>;
     async fn insert(&self, source: NewUser) -> anyhow::Result<User>;
     async fn update(&self, source: UpdateUser) -> anyhow::Result<User>;
+    async fn delete(&self, id: &Id<User>) -> anyhow::Result<Option<User>>;
 }
